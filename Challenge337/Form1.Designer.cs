c554db62151b0f146006dde453fcaed88456ded8
Richard Chin <richardchinloy@hotmail.com>
2017-10-27 00:15:10 +0100

    fully working

--- Challenge337/Form1.Designer.cs
+++ Challenge337/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,7 +48,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(95, 12);
+            this.button2.Location = new System.Drawing.Point(94, 14);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(86, 23);
             this.button2.TabIndex = 1;
@@ -65,11 +67,33 @@
             this.pictureBox.TabIndex = 2;
             this.pictureBox.TabStop = false;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(418, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(186, 14);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Descample 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(505, 460);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -86,6 +110,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Challenge337/Form1.cs
+++ Challenge337/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -96,6 +97,88 @@ namespace Challenge337
             }
         }
 
+        private bool IsGreyscale(Color c) => ((c.R == c.G) && (c.R == c.B));
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int imagewidth = _srcImage.Width;
+            int imageheight = _srcImage.Height;
+
+            using (Graphics graphics = pictureBox.CreateGraphics())
+            using (Bitmap finalimage = new Bitmap(imagewidth, imageheight))
+            using (Bitmap sortedimage = new Bitmap(imagewidth, imageheight))
+            using (Pen pen = new Pen(Color.OrangeRed))
+            using (Pen hpen = new Pen(Color.Crimson))
+            {
+                // clear output
+                graphics.FillRectangle(new SolidBrush(Color.White), pictureBox.ClientRectangle);
+
+                for (int y = 0; y < imageheight; y++)
+                {
+                    // find the x-index of the start of our key colour
+                    int p = 0;
+                    for (; p < imagewidth; p++)
+                    {
+                        // check for non-grey pixel
+                        if (IsGreyscale(_srcImage.GetPixel(p, y)))
+                            continue;
+
+                        if ((p + 1 >= imagewidth) || (p + 2 >= imagewidth))
+                            break;
+
+                        if (!IsGreyscale(_srcImage.GetPixel(p + 1, y)) && !IsGreyscale(_srcImage.GetPixel(p + 2, y)))
+                            break;
+                    }
+
+                    // we should now have the position of our key colour (red).
+                    // Check that we have found it
+                    Debug.Assert(p < imagewidth);
+                    for (int x = p; x < (p + imagewidth); x++)
+                    {
+                        finalimage.SetPixel((x - p), y, _srcImage.GetPixel((x % imagewidth), y));
+                    }
+                    graphics.DrawLine(pen, 0, y, imagewidth - 1, y);
+                }
+
+                graphics.DrawImage(finalimage, 0, 0);
+
+                // sort by key colour red -> green
+                // ((255, 0, _), (254, 1, _), ..., (1, 254, _), (0, 255, _)).
+                var lines = new List<KeyValuePair<Color, int>>();
+                for (int yA = 0; yA < imageheight; yA++)
+                    lines.Add( new KeyValuePair<Color, int>(finalimage.GetPixel(0, yA), yA) );
+
+                int sY = 0;
+                foreach (var line in lines.OrderBy(key => key.Key.R).ThenBy(key => key.Key.G))
+                {
+                    for (int x = 0; x < imagewidth; x++)
+                    {
+                        sortedimage.SetPixel(x, sY, finalimage.GetPixel(x, line.Value));
+                    }
+                    sY++;
+
+                    graphics.DrawLine(hpen, 0, sY, imagewidth - 1, sY);
+                }
+
+                graphics.DrawImage(sortedimage, 0, 0);
+            } // using
+
+        }
+
+        // save results
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*
+            SaveFileDialog filedialog = new SaveFileDialog();
+            filedialog.Filter = "PNG Format|*png";
+            if(filedialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image.Save(filedialog.FileName, ImageFormat.Png);
+            }*/
+        }
+
         private void Form1_FormClosing(object sender, FormClosingEventArgs e) => _srcImage?.Dispose();
+
+
     }
 }
