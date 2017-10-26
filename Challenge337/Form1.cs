41b1f464bbe21bb19082bd53f929a3af43999572
Richard Chin <richardchinloy@hotmail.com>
2017-10-27 00:22:53 +0100

    comments

--- Challenge337/Form1.cs
+++ Challenge337/Form1.cs
@@ -1,4 +1,41 @@
-﻿using System;
+﻿/************************************************************************************************************
+ * Coding challenge 337 from Reddit
+ * 
+ *
+    [2017-10-26] Challenge #337 [Intermediate] Scrambled images
+    #Description
+    For this challenge you will get a couple of images containing
+    a secret word, you will have to unscramble the images to be able to read the words.  
+    To unscramble the images you will have to line up all non-gray scale pixels on each "row" of the image.
+
+    #Formal Inputs &amp; Outputs
+    You get a [scrambled](http://i.imgur.com/rMYBq14.png) image,
+    which you will have to unscramble to get the [original](http://i.imgur.com/wKaiHpv.png) image.
+
+    ###Input description
+    Challenge 1:  [input](http://i.imgur.com/F4SlYMn.png)  
+    Challenge 2: [input](http://i.imgur.com/ycDwgXA.png)  
+    Challenge 3: [input](http://i.imgur.com/hg9iVXA.png)
+
+    ###Output description
+    You should post the correct images or words.
+
+    #Notes/Hints
+    The colored pixels are red (#FF0000, rgb(255, 0, 0)) 
+
+    #Bonus
+    Bonus: [input](http://i.imgur.com/HLc1UHv.png)
+
+    This image is scrambled both horizontally and vertically.  
+    The colored pixels are a gradient from green to red ((255, 0, _), (254, 1, _), ..., (1, 254, _), (0, 255, _)).
+
+    #Finally
+    Have a good challenge idea?
+    Consider submitting it to /r/dailyprogrammer_ideas
+    https://www.reddit.com/r/dailyprogrammer/comments/78twyd/20171026_challenge_337_intermediate_scrambled/
+ *
+ *************************************************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,7 +52,7 @@ namespace Challenge337
 {
     public partial class Form1 : Form
     {
-        private Bitmap _srcImage = null;
+        private Bitmap _srcImage = null;        // Bitmap to hold loaded image
 
         public Form1()
         {
@@ -41,14 +78,6 @@ namespace Challenge337
                 MessageBox.Show("Image not supported", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            catch(FileNotFoundException)
-            {
-                return;
-            }
-            catch(ArgumentException)
-            {
-                return;
-            }
 
             // draw image to screen
             using (Graphics graphics = pictureBox.CreateGraphics())
@@ -57,9 +86,10 @@ namespace Challenge337
             }
         }
 
-        // Descramble
+        // Descramble 1
         // Line up the red pixels
         //
+        // This only works on red pixel scrambled images
         private void button2_MouseClick(object sender, MouseEventArgs e)
         {
             int imagewidth = _srcImage.Width;
@@ -97,8 +127,12 @@ namespace Challenge337
             }
         }
 
+        //
         private bool IsGreyscale(Color c) => ((c.R == c.G) && (c.R == c.B));
 
+        // Full Solution
+        // Works for images scrambled by a non-grey key colour.
+        // We then sort the image lines by R-G scale.
         private void button4_Click(object sender, EventArgs e)
         {
             int imagewidth = _srcImage.Width;
@@ -177,8 +211,7 @@ namespace Challenge337
             }*/
         }
 
+        // clean up when we are closing
         private void Form1_FormClosing(object sender, FormClosingEventArgs e) => _srcImage?.Dispose();
-
-
     }
 }
