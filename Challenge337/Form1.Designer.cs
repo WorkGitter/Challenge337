2307fca5c958e1ebfe50ff635761c37649eeeeb4
Richard Chin <richardchinloy@hotmail.com>
2017-10-26 22:54:53 +0100

    first draft. working

--- Challenge337/Form1.Designer.cs
+++ Challenge337/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Descramble...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(13, 43);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(480, 405);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 460);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox;
     }
 }
 
--- Challenge337/Form1.cs
+++ Challenge337/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,88 @@ namespace Challenge337
 {
     public partial class Form1 : Form
     {
+        private Bitmap _srcImage = null;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        // Open an image and display to the picturebox
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            OpenFileDialog filedialog = new OpenFileDialog();
+            filedialog.Filter = "PNG images|*.png";
+            if (filedialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            // load image and display to 
+            try
+            {
+                _srcImage?.Dispose();
+                _srcImage = (Bitmap)Image.FromFile(filedialog.FileName);
+            }
+            catch (OutOfMemoryException)
+            {
+                MessageBox.Show("Image not supported", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch(FileNotFoundException)
+            {
+                return;
+            }
+            catch(ArgumentException)
+            {
+                return;
+            }
+
+            // draw image to screen
+            using (Graphics graphics = pictureBox.CreateGraphics())
+            {
+                graphics.DrawImage(_srcImage, 0, 0);
+            }
+        }
+
+        // Descramble
+        // Line up the red pixels
+        //
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            int imagewidth = _srcImage.Width;
+            int imageheight = _srcImage.Height;
+            Color keycolour = Color.FromArgb(255, 0, 0);
+
+            using (Graphics graphics = pictureBox.CreateGraphics())
+            using (Bitmap finalimage = new Bitmap(imagewidth, imageheight))
+            using( Pen pen = new Pen(Color.Red))
+            {
+                // clear output
+                graphics.FillRectangle(new SolidBrush(Color.White), pictureBox.ClientRectangle);
+
+                for (int y = 0; y < imageheight; y++)
+                {
+                    // find the x-index of the start of our key colour
+                    int p = 0;
+                    for (; p < imagewidth; p++)
+                    {
+                        if (_srcImage.GetPixel(p, y) == keycolour)
+                            break;
+                    }
+
+                    // we should now have the position of our key colour (red).
+                    // Check that we have found it
+                    Debug.Assert(p < imagewidth);
+                    for (int x = p; x < (p + imagewidth); x++)
+                    {
+                        finalimage.SetPixel((x - p), y, _srcImage.GetPixel((x % imagewidth), y));
+                    }
+                    graphics.DrawLine(pen, 0, y, imagewidth - 1, y);
+                }
+
+                graphics.DrawImage(finalimage, 0, 0);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) => _srcImage?.Dispose();
     }
 }
